e601c76a95531f226d14fe4e32ee28b0c6cd597f
dininduwm <dininduwm@gmail.com>
2022-07-15 12:36:22 +0530

    added FPGA intergration files of RV32IM processor

---
+++ RV32IM_pipeline_FPGA/cpu/immediate_select_module/immediate_select.v
@@ -0,0 +1,59 @@
+module immediate_select(INST, SELECT, OUT);
+
+input [31:0] INST;
+input [3:0] SELECT;
+output reg [31:0] OUT;
+
+wire [19:0] TYPE1, TYPE2;
+wire [11:0] TYPE3, TYPE4, TYPE5;
+wire [4:0] TYPE6;
+
+
+// TODO: Check the combinations
+assign TYPE1 = INST[31:12];
+assign TYPE2 = INST[31:12];
+assign TYPE3 = INST[31:20]; 
+assign TYPE4 = {INST[31:25], INST[11:7]};
+assign TYPE5 = {INST[31:25], INST[11:7]};
+assign TYPE6 = INST[29:25]; 
+
+always @(*) begin
+    case (SELECT[2:0])
+        // TYPE 1 
+        3'b000:
+                OUT = {TYPE1, {12{1'b0}}};
+        // TYPE 2 
+        3'b001:
+            if (SELECT[3] == 1'b1) 
+                OUT = {{11{1'b0}}, TYPE2, 1'b0};
+            else
+                // TODO
+                OUT = {{11{INST[31]}}, INST[31], INST[19:12], INST[20], INST[30:21], 1'b0}; //{{11{TYPE2[19]}}, TYPE2, 1'b0};
+        // TYPE 3 
+        3'b010:
+            if (SELECT[3] == 1'b1) 
+                OUT = {{20{1'b0}}, TYPE3};
+            else
+                OUT = {{20{TYPE3[11]}}, TYPE3};
+        // TYPE 4 
+        3'b011:
+            if (SELECT[3] == 1'b1) 
+                // OUT = {{19{1'b0}}, TYPE4, 1'b0};
+                OUT = {{20{1'b0}}, INST[31], INST[7], INST[30:25], INST[11:8], 1'b0};
+            else
+                OUT = {{20{INST[31]}}, INST[31], INST[7], INST[30:25], INST[11:8], 1'b0};
+        // TYPE 5 
+        3'b100:
+            if (SELECT[3] == 1'b1) 
+                OUT = {{20{1'b0}}, TYPE5};
+            else
+                OUT = {{20{TYPE5[11]}}, TYPE5};
+        // TYPE 6 
+        3'b101:
+            OUT = {{27{1'b0}}, TYPE6};
+    endcase
+end
+
+
+
+endmodule
---
+++ RV32IM_pipeline_FPGA/cpu/register_file_module/reg_file.v
@@ -0,0 +1,44 @@
+module reg_file(IN, OUT1, OUT2, INADDRESS, OUT1ADDRESS, OUT2ADDRESS, WRITE, CLK, RESET);
+
+input [31:0] IN;  // 32 bit data input
+input [4:0] INADDRESS, OUT1ADDRESS, OUT2ADDRESS; // 5 bit data inputs
+input WRITE, CLK, RESET; // 1 bit data inputs
+output [31:0] OUT1, OUT2; // 32 bit data outputs
+
+reg [31:0] REGISTERS [31:0]; // 32 bit x 32 register file
+
+
+// TODO : set the time delay
+assign OUT1 = REGISTERS[OUT1ADDRESS]; //writing data to outputs
+assign OUT2 = REGISTERS[OUT2ADDRESS]; //writing data to outputs
+
+always @ (posedge CLK) // this code block run when we are in a positive clock edge
+begin
+    // write input to the in address
+    
+    // TODO : set the time delay
+    // #0.001
+    
+        if (WRITE == 1'b1)
+    begin
+        REGISTERS[INADDRESS] = IN; 
+    end    
+end
+
+
+// combinational logic output for reset signal (level triggered input)
+//integer i;
+//always @ (*)
+//begin
+//    if (RESET == 1'b1)
+//    begin
+//        // TODO : set the time delay 
+//        for (i = 0; i < 32; i=i+1) //looping through register file and setting them to 0s
+//        begin
+//            REGISTERS[i] = 0;
+//        end   
+//    end
+//    
+//end
+
+endmodule
