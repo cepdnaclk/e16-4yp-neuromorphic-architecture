a66a781f53585f2e53457efb6014f105154dae09
dtdinidu7 <dtdinidu7@gmail.com>
2022-11-29 08:43:04 +0530

    floating point instruction helper completed

--- Assembler/FloatInstructionHelper.py
+++ Assembler/FloatInstructionHelper.py
@@ -3,40 +3,6 @@
 
 # Python program to convert a real value
 # to IEEE 754 Floating Point Representation.
- 
-# # Function to convert a
-# # fraction to binary form.
-# def binaryOfFraction(fraction):
-#     binary = str()
-#     while (fraction):
-#         fraction *= 2
-#         if (fraction >= 1):
-#             int_part = 1
-#             fraction -= 1
-#         else:
-#             int_part = 0
-#         binary += str(int_part)
- 
-#     # Returning the binary string.
-#     return binary
- 
-# # Function to get sign  bit,
-# # exp bits and mantissa bits,
-# # from given real no.
-# def floatingPoint(real_no):
-#     sign_bit = 0
- 
-#     if(real_no < 0):
-#         sign_bit = 1
-
-#     real_no = abs(real_no)
-#     int_str = bin(int(real_no))[2 : ]
-#     fraction_str = binaryOfFraction(real_no - int(real_no))
-#     ind = int_str.index('1')
-#     exp_str = bin((len(int_str) - ind - 1) + 127)[2 : ]
-#     mant_str = int_str[ind + 1 : ] + fraction_str
-#     mant_str = mant_str + ('0' * (23 - len(mant_str)))
-#     return str(sign_bit)+exp_str+mant_str
 
 import struct
 
@@ -55,5 +21,6 @@ def genSequence(fNumber, register):
     print('slli {}, {}, {}'.format(register, register, 10))
     print('addi {}, {}, {}'.format(register, register, int(floatString[22:32], 2)))
 
-
-genSequence(234234234234234.33, 'x5')
+r = input("Enter the register n0: ")
+f = input("Enter the floating point number: ")
+genSequence(float(f), r)
