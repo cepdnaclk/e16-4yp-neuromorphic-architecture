a14dfd03e9c2042e44d42a7e4bb6770195885b00
dtdinidu7 <dtdinidu7@gmail.com>
2023-01-11 13:20:57 +0530

    .

--- Assembler/network_fifo_test.s
+++ Assembler/network_fifo_test.s
@@ -1,13 +1,10 @@
 nop
-addi x2, x2, 31
+addi x2, x2, 10
 swnet x2, x0(32)
-swnet x2, x0(32)
-swnet x2, x0(32)
-swnet x2, x0(32)
-
-swnet x2, x0(32)
-swnet x2, x0(32)
-swnet x2, x0(32)
+addi x2, x2, 10
+swnet x2, x0(35)
+addi x2, x2, 10
+swnet x2, x0(38)
 swnet x2, x0(32)
 
 swnet x2, x0(32)
@@ -24,3 +21,12 @@ swnet x2, x0(32)
 swnet x2, x0(32)
 swnet x2, x0(32)
 swnet x2, x0(32)
+
+swnet x2, x0(32)
+
+lwnet x3, x0(0)
+lwnet x4, x0(1)
+lwnet x3, x0(0)
+lwnet x4, x0(1)
+lwnet x3, x0(0)
+lwnet x4, x0(1)
